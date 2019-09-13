63ebb308c3fe0591ce68b4615c6a7beb3543c72a
Mihai Dobri <dobrimihai@yahoo.com>
2019-09-13 14:21:10 -0500

    add Size exercise

--- IntermediatePythonForDataScience/Matplotlib/04_Labels&Ticks.py
+++ IntermediatePythonForDataScience/Matplotlib/04_Labels&Ticks.py
@@ -11,7 +11,7 @@ title = 'World Development in 2007'
 plt.xlabel(xlab)
 plt.ylabel(ylab)
 
-#Exercise 2 : Ticks
+###Exercise 2 : Ticks
 # Scatter plot
 plt.scatter(gdp_cap, life_exp)
 
@@ -29,4 +29,27 @@ tick_lab = ['1k', '10k', '100k']
 plt.xticks(tick_val,tick_lab)
 
 # After customizing, display the plot
+plt.show()
+
+### Exercise 3 : Size
+# Import numpy as np
+import numpy as np
+
+# Store pop as a numpy array: np_pop
+np_pop=np.array(pop)
+
+# Double np_pop
+np_pop=np_pop*2
+
+# Update: set s argument to np_pop
+plt.scatter(gdp_cap, life_exp, s = np_pop)
+
+# Previous customizations
+plt.xscale('log')
+plt.xlabel('GDP per Capita [in USD]')
+plt.ylabel('Life Expectancy [in years]')
+plt.title('World Development in 2007')
+plt.xticks([1000, 10000, 100000],['1k', '10k', '100k'])
+
+# Display the plot
 plt.show()
