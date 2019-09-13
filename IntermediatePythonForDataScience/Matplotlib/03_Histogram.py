eb3f15071361bc71790b7f5f849c8858681732e1
Mihai Dobri <dobrimihai@yahoo.com>
2019-09-12 22:18:51 -0500

    add exercise for ScatterPlot

---
+++ IntermediatePythonForDataScience/Matplotlib/03_Histogram.py
@@ -0,0 +1,28 @@
+# Build histogram with 5 bins
+plt.hist(life_exp,5)
+
+# Show and clean up plot
+plt.show()
+plt.clf()
+
+# Build histogram with 20 bins
+plt.hist(life_exp,20)
+
+# Show and clean up again
+plt.show()
+plt.clf()
+
+#Exercise 2
+# Histogram of life_exp, 15 bins
+plt.hist(life_exp,15)
+
+# Show and clear plot
+plt.show()
+plt.clf()
+
+# Histogram of life_exp1950, 15 bins
+plt.hist(life_exp1950,15)
+
+# Show and clear plot again
+plt.show()
+plt.clf()
