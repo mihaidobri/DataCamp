87ee8c55632fef8bfb8421078c561f9bb217f488
Mihai Dobri <dobrimihai@yahoo.com>
2019-09-12 22:16:00 -0500

    add exercise for LinePlot

---
+++ IntermediatePythonForDataScience/Matplotlib/01_LinePlot.py
@@ -0,0 +1,25 @@
+# Print the last item from year and pop
+print(year[-1])
+print(pop)
+
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+plt.plot(year,pop)
+
+# Display the plot with plt.show()
+plt.show()
+
+#Exercise 2
+# Print the last item of gdp_cap and life_exp
+print(gdp_cap[-1])
+print(life_exp[-1])
+
+
+# Make a line plot, gdp_cap on the x-axis, life_exp on the y-axis
+plt.plot(gdp_cap,life_exp)
+
+# Display the plot
+plt.show()
